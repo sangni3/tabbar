270b6eaca4cfe0214d4f807fb46e163704508482
sangni3 <1416686545@qq.com>
2020-10-29 17:26:45 +0800

    '测试2'

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <router-view></router-view>
     <tabbar>
       <tabbar-item path="/home">
-        <img slot="item-icon" src="assets/img/tabbar/home.svg" alt="">
-        <img slot="item-icon-active" src="assets/img/tabbar/home(1).svg" alt="">
+        <img slot="item-icon" src="./assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/home(1).svg" alt="">
         <div slot="item-text">首页</div>
       </tabbar-item>
       <tabbar-item path="/category">
@@ -13,13 +13,13 @@
         <div slot="item-text">分类</div>
       </tabbar-item>
       <tabbar-item path="/cart">
-        <img slot="item-icon" src="assets/img/tabbar/shop.svg" alt="">
-        <img slot="item-icon-active" src="assets/img/tabbar/shop(1).svg" alt="">
+        <img slot="item-icon" src="./assets/img/tabbar/shop.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/shop(1).svg" alt="">
         <div slot="item-text">购物车</div>
       </tabbar-item>
       <tabbar-item path="/profile" >
-        <img slot="item-icon" src="assets/img/tabbar/profile.svg" alt="">
-        <img slot="item-icon-active" src="assets/img/tabbar/profile(1).svg" alt="">
+        <img slot="item-icon" src="./assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/profile(1).svg" alt="">
         <div slot="item-text">我的</div>
       </tabbar-item>
     </tabbar>
@@ -28,8 +28,8 @@
 </template>
 
 <script>
-import Tabbar from 'components/tabbar/tabbar'
-import tabbarItem from "components/tabbar/tabbar-item"
+import Tabbar from './components/tabbar/tabbar'
+import tabbarItem from "./components/tabbar/tabbar-item"
 export default {
   name: 'App',
   components:{
